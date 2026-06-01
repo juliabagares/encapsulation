c413210e813693f37aee2f63e2dec8d63d6b1b70
Julia Bagares <juliabagares2007@gmail.com>
2026-06-01 23:11:37 +0800

    speed is 0 and display

--- car_program/car.py
+++ car_program/car.py
@@ -1,11 +1,19 @@
 class Car:
-    def __init__(self, year_model, make, speed):
+    def __init__(self, year_model, make):
         self.__year_model = year_model
         self.__make = make
-        self.__speed = speed
+        self.__speed = 0
 
     def accelerate(self):
         self.__speed += 5
 
     def brake(self):
         self.__speed -= 5
+
+    def get_speed(self):
+        return self.__speed
+
+    def display(self):
+        print(f"{self.__year_model} {self.__make} | Speed: {self.__speed} km/h")
+
+    
